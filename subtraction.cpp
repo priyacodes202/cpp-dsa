61930869e3fbc6c93c41f22f4f9ade4097796242
priyacodes202 <priyatelegareddy@gmail.com>
2026-08-13 18:03:51 +0530

    Add subtraction program in C++

---
+++ subtraction.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int first,second,subtract;
+cout<<"enter your first number:";
+cin>>first;
+cout<<"enter your second number:";
+cin>>second;
+subtract=first-second;
+cout<<"subtract="<<subtract;
+return 0;
+}
