a2e8db8d090d4706ac15644d26213358797c469c
priyacodes202 <priyatelegareddy@gmail.com>
2026-08-13 18:17:39 +0530

    Implement multiplication program in C++

---
+++ multplication.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int first,second,multiplication;
+cout<<"enter your first number:";
+cin>>first;
+cout<<"enter your second number:";
+cin>>second;
+multiply=first*second;
+cout<<"multiply="<<multiply;
+return 0;
+}
