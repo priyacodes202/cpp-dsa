c34bc3551d4df7c71da0bb56f2f93519ae039263
priyacodes202 <priyatelegareddy@gmail.com>
2026-08-13 17:59:24 +0530

    Fix include statement and implement area calculation

---
+++ area of rectangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int  main() {
+  int length,breadth,area;
+cout<<"enter your length:";
+cin>>length;
+cout<<"enter your breadth:";
+cin>>breadth;
+area =length*breadth;
+cout<<"area="<<area;
+return 0;
+}
