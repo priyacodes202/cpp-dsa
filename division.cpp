1b2890829404f62d97c1888c21a67d72a9d7fd14
priyacodes202 <priyatelegareddy@gmail.com>
2026-08-13 18:20:55 +0530

    Add division program in C++

---
+++ division.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int first,second,division;
+cout<<"enter your first number:";
+cin>>first;
+cout<<"enter your second number:";
+cin>>second;
+division=first/second;
+cout<<"division="<<division;
+return 0;
+}
