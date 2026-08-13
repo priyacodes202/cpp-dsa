5fbae69f35ad1dfbd4585781530de2cad6060af1
priyacodes202 <priyatelegareddy@gmail.com>
2026-08-13 18:24:58 +0530

    Add program to calculate remainder of two numbers

---
+++ remainder.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int first,second,remainder;
+cout<<"enter your first number:";
+cin>>first;
+cout<"enter your second number:";
+cin>>second;
+remainder=first%second;
+cout<<"remainder="<<remainder;
+return 0;
+}
