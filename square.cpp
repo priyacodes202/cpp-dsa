35274e36b2ce2dcb5986dd7e5fd6c61c47afe444
priyacodes202 <priyatelegareddy@gmail.com>
2026-08-13 17:54:32 +0530

    Implement square calculation in square.cpp
    Fixes include directive and return statement, and corrects output stream syntax.

---
+++ square.cpp
@@ -0,0 +1,10 @@
+#include<iosteam>
+using namespace std;
+int main() {
+  int number,square;
+cout<<"enter your number:";
+cin>>number;
+square=number*number;
+cout<<"square="<<square;
+retuen 0;
+}
