0ad5d7c0078b355feae70fe4bc93356ccf04eded
priyacodes202 <priyatelegareddy@gmail.com>
2026-08-13 17:51:16 +0530

    Add basic addition program in C++

---
+++ addtion.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int first,second,add;
+cout<<"enter your first number:";
+cin>>first;
+cout<<"enter your second number:";
+cin>>second;
+add =first+second;
+cout<<"add="<<add;
+return 0;
+}
+  
