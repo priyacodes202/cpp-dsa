f06bef35826fd99ba22c776eccb31f15ab2da799
priyacodes202 <priyatelegareddy@gmail.com>
2026-08-15 14:43:31 +0530

    Implement even-odd number checker in C++

---
+++ even-odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namesapce std;
+int main() {
+  int number;
+  cout<<"enter your number:";
+  cin>>number;
+  if (number%2==0){
+   cout<<"even";
+  }
+  else {
+    cout<<"odd";
+  }
+  return 0;
+}
