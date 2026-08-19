dac718d77f9614fabd6f16672a2995d79feebab2
priyacodes202 <priyatelegareddy@gmail.com>
2026-08-19 19:50:28 +0530

    Implement sum calculation using a while loop
    Fix variable name and correct sum calculation.

---
+++ sum-while.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n,i=1,sum=0;
+    cout<<"enter your number:";
+    cin>>number;
+    while(i<=n){
+        sum=sum+i;
+        i++;
+    }
+    cout<<"sum="<<sum;
+    return 0;
+}
