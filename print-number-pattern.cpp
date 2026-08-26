589e0729f639015a6f77cfe3dd0650285a8bb6c4
priyacodes202 <priyatelegareddy@gmail.com>
2026-08-26 10:27:12 +0530

    Implement number pattern printing in C++

---
+++ print-number-pattern.cpp
@@ -0,0 +1,11 @@
+#include<iostrem>
+using namespace std;
+int main() {
+    for(int i=1;i<=10;i++){
+       for(int j=1;j<=9;j++){
+          cout<<j<<"";
+       }
+       cout<<endl;
+    }
+    return 0;
+}
