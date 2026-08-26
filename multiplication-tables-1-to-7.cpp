492997b06c3b6623e84dd3fb956c75eb6287db6f
priyacodes202 <priyatelegareddy@gmail.com>
2026-08-26 11:08:09 +0530

    Add multiplication tables from 1 to 7

---
+++ multiplication-tables-1-to-7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    for(int i=1;i<=7;i++){
+       for(int j=1;j<=15;j++){
+          cout<<i<<"x"<<j<<"="<<i*j<<endl;
+       }
+       cout<<endl;
+    }
+    return 0;
+}
