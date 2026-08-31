53463f8389ceed4d5484aef44d78755989d2e033
priyacodes202 <priyatelegareddy@gmail.com>
2026-08-31 15:24:55 +0530

    Implement even-odd check function in C++

---
+++ even-odd-function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+voidCheckEvenOdd(int numbers){
+    if(number %2==0){
+       cout<<"Even";
+    }
+    else{
+       cout<<"Odd";
+    }
+}
+int main(){
+   CheckEvenOdd(4);
+   return 0;
+}
